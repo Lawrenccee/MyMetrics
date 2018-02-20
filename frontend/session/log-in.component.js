667a687adc66af3308c663da131e091ee4fa9503
Lawrence <lawrenccee@gmail.com>
2018-02-20 13:18:29 -0800

    Able to send API requests to backend

--- frontend/session/log-in.component.js
+++ frontend/session/log-in.component.js
@@ -2,9 +2,18 @@ angular.
   module('logIn').
   component('logIn', {
     templateUrl: 'session/log-in.template.html',
-    controller: function($scope) {
+    controller: function($scope , $http) {
+
+      $http({
+        method: "GET",
+        url: "api/users"
+      }).then((users) => {
+        console.log(users);
+      });
+
       $scope.sendUser = () => {
         console.log($scope.user);
       };
     }
   });
+
