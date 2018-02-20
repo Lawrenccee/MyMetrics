5181b6862fb36082d98128b1a71215c0e7530dec
Samuel Chia <cheeeya@berkeley.edu>
2018-02-20 11:16:15 -0800

    remove mongoose from server.js

--- backend/lib/server.js
+++ backend/lib/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import passport from 'passport';
 import session from 'express-session';
-import mongoose from 'mongoose';
 import { passportConfig } from './passport.js';
 import { routerConfig } from './routes.js';
 
