24dccc4585d05b11cd737eaebc8f0f681a20b2a7
BertShin <bertshin08@gmail.com>
2018-02-20 19:30:56 -0800

    Comment out Routes for now until concrete Routes are decided upon

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -4,25 +4,25 @@
 // path. (e.g index.html#!/some/path)
 // This is not necessary but considered good practice. and 
 // '!' is the most commonly used prefix.
-angular.
-    module('myMetricsApp').
-    config(['$locationProvider', '$routeProvider',
-        function config($locationProvider, $routeProvider) {
-            $locationProvider.hashPrefix('!');
+// angular.
+//     module('myMetricsApp').
+//     config(['$locationProvider', '$routeProvider',
+//         function config($locationProvider, $routeProvider) {
+//             $locationProvider.hashPrefix('!');
 
-            $routeProvider.
-                when('/doctorsignup', {
-                    template: '<doctor-sign-up></doctor-sign-up>'
-                }).
-                when('/patientsignup', {
-                    template: '<patient-sign-up></patient-sign-up'
-                }).
-                when('/login', {
-                    template: '<log-in></log-in>'
-                }).
-                otherwise('/#!');
-        }
-    ]);
+//             $routeProvider.
+//                 when('/doctorsignup', {
+//                     template: '<doctor-sign-up></doctor-sign-up>'
+//                 }).
+//                 when('/patientsignup', {
+//                     template: '<patient-sign-up></patient-sign-up'
+//                 }).
+//                 when('/login', {
+//                     template: '<log-in></log-in>'
+//                 }).
+//                 otherwise('/#!');
+//         }
+//     ]);
 
 
 
