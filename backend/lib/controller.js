d63ce69eca7a0ec0c3889c0f866689d693b1d34e
Samuel Chia <cheeeya@berkeley.edu>
2018-02-21 10:30:55 -0800

    add timestamp to log schema

--- backend/lib/controller.js
+++ backend/lib/controller.js
@@ -33,10 +33,11 @@ export const createUser = (req, res) => {
   mongoose.connect(process.env.MONGODB_URI).then(
     () => {
       let user = new User(req.body.user);
-      User.findOne({ email: 'test0@test.com' }, (err, u) => {
-        user.patients.push(u);
-        user.save().then(r => res.send(r), err => res.send(err));
-      });
+      // User.findOne({ email: 'test0@test.com' }, (err, u) => {
+      //   user.patients.push(u);
+      //   user.save().then(r => res.send(r), err => res.send(err));
+      // });
+      user.save().then(u => res.send(u), e => res.send(e));
       // User.create(user, (err, u) => {
       //   if (err) res.send(err);
       //   if (u) res.send(u);
