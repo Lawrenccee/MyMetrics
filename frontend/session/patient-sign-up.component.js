1d90f45a40bd7816a103d08bbb7090544d32ccec
Samuel Chia <cheeeya@berkeley.edu>
2018-02-20 20:53:14 -0800

    remove log statement and convert to single line return

--- frontend/session/patient-sign-up.component.js
+++ frontend/session/patient-sign-up.component.js
@@ -3,15 +3,13 @@ angular.
   component('patientSignUp', {
     templateUrl: 'session/patient-sign-up.template.html',
     controller: function ($http) {
-      this.sendUser = () => {
-        console.log(this.user);
-        return (
-          $http({
-            method: "POST",
-            url: '/api/users',
-            data: { user: this.user }
-          })
-        )
-      }
+      this.sendUser = () =>
+      (
+        $http({
+          method: "POST",
+          url: '/api/users',
+          data: { user: this.user }
+        })
+      )
     }
   });
