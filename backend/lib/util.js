3cb9dfa57de090c6c25264887dd1bff6b925c190
Lawrence <lawrenccee@gmail.com>
2018-02-20 18:31:15 -0800

    remove util

--- backend/lib/util.js
+++
@@ -1,8 +0,0 @@
-export const connectionString = process.env.DATABASE_URL || 'mongodb://mymetrics_tester:metricstest123@ds227168.mlab.com:27168/my_metrics';
-
-export const googleConfig = {
-  clientID: '50422910844-k6mahl2eal1ntpnoeunbevg753o5f5i0.apps.googleusercontent.com',
-  clientSecret: 'A2YtKP0JVpPK9DZu7Rb4rZQ',
-  callbackURL: 'http://localhost:3000/auth/google/callback'
-};
-
