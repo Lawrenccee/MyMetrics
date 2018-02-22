83a2e5574b8dfe377ffb813f893d76ffa643584b
Lawrence <lawrenccee@gmail.com>
2018-02-22 09:24:09 -0800

    Remove commennts in app module

--- frontend/app.module.js
+++ frontend/app.module.js
@@ -1,15 +1,2 @@
-const app = angular.module('myMetricsApp', ['ngRoute', 'logIn', 'patientSignUp', 'doctorSignUp', 'patientView', 'doctorView']);
+angular.module('myMetricsApp', ['ngRoute', 'logIn', 'patientSignUp', 'doctorSignUp', 'patientView', 'doctorView']);
 
-// app.run(['$rootScope', '$location', 'UserService', function ($rootScope, $location, UserService) {
-//   $rootScope.$on('$routeChangeStart', function (event) {
-//     let user = JSON.parse(UserService.getStore());
-//     // console.log(user);
-
-//     if (!user || !user.email) {
-//       $location.path('/login');
-//     }
-//     // else {
-//     //   $location.path('/patientview');
-//     // }
-//   });
-// }]);
