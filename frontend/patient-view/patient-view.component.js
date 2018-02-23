bfb2edfa6cc0ecab284bd470901185fae18156be
Lawrence <lawrenccee@gmail.com>
2018-02-23 08:50:02 -0800

    Add logic for checking vitals, changing next appointment, grabbing data for the day selected

--- frontend/patient-view/patient-view.component.js
+++ frontend/patient-view/patient-view.component.js
@@ -13,6 +13,11 @@ angular.
           url: `/api/users/${this.patient.id}`
         }).then((res) => {
           this.patient = res.data;
+          this.patient.nextAppt = new Date().setDate(new Date().getDate() + 1);
+          if (new Date() < this.patient.nextAppt) {
+            // MIGHT HAVE TO CONVERT FROM MILLISECONDS TO DATE
+            this.nextAppt = this.patient.nextAppt;
+          }
           this.patient.symptoms = [];
 
           createChart(this.patient);
@@ -55,7 +60,7 @@ angular.
       };
 
       this.date = new Date();
-      this.nextAppt = new Date();
+      this.nextAppt = undefined;
 
       this.updatePatient = () => {
         this.patient.date = new Date(
@@ -86,6 +91,27 @@ angular.
           UserService.clear();
           $window.location.href = '#!/login';
         });
+      };
+
+      this.changeDate = () => {
+        console.log(this.date);
+        let dateMs = new Date(
+          new Date().setHours(0, 0, 0, 0)).
+          setFullYear(
+            this.date.getFullYear(),
+            this.date.getMonth(),
+            this.date.getDate()
+          );
+        
+        // HTTP REQUEST TO CHANGE LOGS
+        // in the then()
+        // this.patient.weight = res.data.weightEntry ? res.data.weightEntry[1] : undefined;
+        // this.patient.sodium = res.data.sodiumEntry ? res.data.sodiumEntry[1] : undefined;
+        // this.patient.fluid = res.data.fluidEntry ? res.data.fluidEntry[1] : undefined;
+      };
+
+      this.changeNextAppt = () => {
+        this.patient.nextAppt = this.nextAppt;
       };
 
       this.symptoms = [
@@ -121,6 +147,49 @@ angular.
           this.patient.medications.splice(index, 1);
         }
       };
+
+      const checkVitals = () => {
+        // CALL THIS METHOD WHEN UPDATING PATIENT, CALL WHEN INIT
+        let today = new Date().setHours(0, 0, 0, 0);
+
+        let yday = new Date();
+        yday.setDate(yday.getDate() - 1);
+        yday = yday.setHours(0, 0, 0, 0);
+
+        let weekAgo = new Date();
+        weekAgo.setDate(weekAgo.getDate() - 7);
+        weekAgo = weekAgo.setHours(0, 0, 0, 0);
+
+        console.log(new Date(today));
+        console.log(new Date(yday));
+        console.log(new Date(weekAgo));
+
+        // // fetch weight from previous day
+        // // if weight of today - yday is >= 2 warn
+        // if (this.patient.weight - ydayWeight >= 2) {
+        //   this.warnings.push("You have gained 2 or more pounds since yesterday");
+        // }
+
+        // // fetch weight from a week ago
+        // // if weight of today - week ago is >= 7 warn
+        // if (this.patient.weight - weekAgoWeight >= 5) {
+        //   this.warnings.push("You have gained 5 or more pounds in a week");
+        // }
+
+        // // look at todays fluids
+        // // if more than 2 liters warn
+        // if (this.patient.fluid >= 2) {
+        //   this.warnings.push("Your fluids are over 2 Liters for the day");
+        // }
+
+        // // look at todays sodium
+        // // if more than ... warn
+        // if (this.patient.sodium >= ???) {
+        //   this.warnings.push("Your sodium is over *** for the day");
+        // }
+      };
+
+      checkVitals();
 
       const createChart = ({ weightLog, sodiumLog, fluidLog }) => {
         Highcharts.chart('graph', {
