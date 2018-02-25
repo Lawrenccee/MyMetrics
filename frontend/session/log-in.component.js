178967290a190de9b29e6c5a6caf967e97694af4
Lawrence <lawrenccee@gmail.com>
2018-02-25 12:30:01 -0800

    Change loader color to orange

--- frontend/session/log-in.component.js
+++ frontend/session/log-in.component.js
@@ -3,7 +3,7 @@ angular.
   component('logIn', {
     templateUrl: 'session/log-in.template.html',
     controller: function(UserService, $http, $window) {
-      this.loading = true;
+      this.loading = false;
 
       this.sendUser = () => {
         this.loading = true;        
